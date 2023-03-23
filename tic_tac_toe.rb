ef5865fd307c14c8b427972cd7448b8fe6d903f5
LughsClues <cuberfreak@gmail.com>
2023-03-23 12:32:22 +0100

    Fix LINES

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,7 +2,7 @@
 
 # Runs the TicTacToe Game
 class TicTacToe
-  LINES = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6]].freeze
+  LINES = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]].freeze
   
   def initialize(squares = Array.new(9, ' '))
     @squares = squares
