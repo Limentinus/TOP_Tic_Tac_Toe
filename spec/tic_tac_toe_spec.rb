608bd36cbc5871f2eb450d399d39268555bd1399
LughsClues <cuberfreak@gmail.com>
2023-03-23 11:10:05 +0100

    Second test

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -2,11 +2,19 @@ require_relative '../tic_tac_toe'
 
 describe TicTacToe do
   describe '#won?' do
-    subject(:test_game) {TicTacToe.new(['X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' '])}
-
+    
     context 'When three squares in a line are the same mark' do
+      subject(:first_row_game) {TicTacToe.new(['X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' '])}
       it 'returns true for top row' do
-        expect(test_game.won?('X')).to eq(true)
+        expect(first_row_game.won?('X')).to eq(true)
+      end
+    end
+
+    context "When three squares in a different line are a different mark" do
+      subject(:diagonal_game) { described_class.new(['O', ' ', ' ', ' ', 'O', ' ', ' ', ' ', 'O', ' ']) }
+
+      it 'returns true for the diagonal line' do
+        expect(diagonal_game.won?('O')).to eq(true)
       end
     end
   end
